6bcca13c4639dac72096507dfcb7ae04b7e69619
Hkaar <HkaarDev@gmail.com>
2026-06-15 23:39:16 +0800

    chore: removed old

--- opera/app/[locale]/(app)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AppShell from "@/app/components/shared/AppShell";
-
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="min-h-screen bg-canvas text-body"><AppShell>{children}</AppShell></div>;
-}
--- opera/app/[locale]/(app)/session/[id]/council/loading.tsx
+++
@@ -1,5 +0,0 @@
-import SessionLoader from '@/app/components/shared/SessionLoader';
-
-export default function Loading() {
-  return <SessionLoader />;
-}
